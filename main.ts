1136f1d3c2bcf75d64a8af770a3d69d6597dbcaf
RodainaAhmed <136331463+RodainaAhmed@users.noreply.github.com>
2023-07-02 20:58:07 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    counter += 3
+    basic.showNumber(counter)
+})
+input.onButtonPressed(Button.AB, function () {
+    counter = 0
+})
+input.onButtonPressed(Button.B, function () {
+    counter += 1
+    basic.showNumber(counter)
+})
+let counter = 0
+counter = 0
+basic.showNumber(counter)
