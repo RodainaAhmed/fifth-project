a6da4bb6a3fa89b6a1a183801ccc59ec10b00063
RodainaAhmed <136331463+RodainaAhmed@users.noreply.github.com>
2023-07-02 21:04:24 +0300

    updated

--- main.ts
+++ main.ts
@@ -1,11 +1,14 @@
 input.onButtonPressed(Button.A, function () {
+    counter = 0
     counter += 3
     basic.showNumber(counter)
 })
 input.onButtonPressed(Button.AB, function () {
     counter = 0
+    basic.showNumber(counter)
 })
 input.onButtonPressed(Button.B, function () {
+    counter = 0
     counter += 1
     basic.showNumber(counter)
 })
